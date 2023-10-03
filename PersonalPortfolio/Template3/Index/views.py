bb6cf1b556f7c7ad0f936e8eb0c20a6c5a75e474
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-03 18:15:29 +0545

    Initial

--- PersonalPortfolio/Template3/Index/views.py
+++ PersonalPortfolio/Template3/Index/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(reuest):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    return render(reuest,"index.html")
