f9eaeb562047e327459d97eae755a6833d23b75c
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-03 18:21:43 +0545

    Initial

--- PersonalPortfolio/Template3/Index/admin.py
+++ PersonalPortfolio/Template3/Index/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class ContactUsDisplay(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message')
+
+#registering the model
+admin.site.register(ContactForm, ContactUsDisplay)
--- PersonalPortfolio/Template3/Index/models.py
+++ PersonalPortfolio/Template3/Index/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ContactForm(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    
+    class Meta:
+        verbose_name = "Contact Form"
+        verbose_name_plural = "Contact Forms"
