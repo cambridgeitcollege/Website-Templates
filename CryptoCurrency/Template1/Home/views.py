3ee2228743576920723f75eb802664a5611adaa9
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-16 19:23:26 +0545

    Initial

--- CryptoCurrency/Template1/Home/views.py
+++ CryptoCurrency/Template1/Home/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render,HttpResponse,redirect
 def index(request):
     # return HttpResponse("Hello World")
     return render(request,'index.html')
+def about(request):
+    return render(request,"about.html")
+
+def services(request):
+    return render(request,"services.html")
+
+def whyus(request):
+    return
