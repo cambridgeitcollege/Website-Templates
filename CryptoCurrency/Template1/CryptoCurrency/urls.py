c4f97c626df54c37d29a69f5ee0ce4d4ccfd18c2
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-16 17:55:57 +0545

    Initial

--- CryptoCurrency/Template1/CryptoCurrency/urls.py
+++ CryptoCurrency/Template1/CryptoCurrency/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from Home import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('')
+    path("/",include('Home.urls')),
 ]
--- CryptoCurrency/Template1/Home/urls.py
+++ CryptoCurrency/Template1/Home/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('')
+    path("/",views.index,name="Index"),
 ]
